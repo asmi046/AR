43b087e1c6469ed5a737794e426aaf8d74c17728
asmi046 <asmi046@gmail.com>
2024-03-05 13:57:37 +0300

    +

--- resources/views/dimon.blade.php
+++ resources/views/dimon.blade.php
@@ -34,9 +34,9 @@
           position="-10 0 0" sound="src: url({{ Storage::url($element->sound) }}); autoplay: false;"></a-entity> --}}
 
        <a-entity
-          position="0 0 0"
+          position="-1 0 0"
           scale="1 1 1"
-          rotation="-99 0 0"
+          rotation="-99 10 0"
           gltf-model="{{asset('models/dimon.glb')}}"
         ></a-entity>
 
